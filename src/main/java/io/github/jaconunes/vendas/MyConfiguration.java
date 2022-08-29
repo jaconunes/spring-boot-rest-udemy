94c70c7367490f2040578875b313f6bfbeadf3b5
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2022-08-29 19:35:40 -0300

    Commit inicial do projeto de estudos Spring Boot - Java

---
+++ src/main/java/io/github/jaconunes/vendas/MyConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.jaconunes.vendas;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+    @Bean
+    public String applicationName(){
+        
+        return "Sistema de Vendas";
+    }
+
+}

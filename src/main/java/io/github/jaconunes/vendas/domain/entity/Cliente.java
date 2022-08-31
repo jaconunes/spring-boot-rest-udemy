3134e515402225bf9690dd20d066d4aa186a8d0f
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2022-08-30 21:18:10 -0300

    atualizando com as alterações da aula 19

--- src/main/java/io/github/jaconunes/vendas/domain/entity/Cliente.java
+++ src/main/java/io/github/jaconunes/vendas/domain/entity/Cliente.java
@@ -1,12 +1,16 @@
-package io.github.jaconunes.vendas.model;
+package io.github.jaconunes.vendas.domain.entity;
 
 public class Cliente {
 
+    private Integer id;
     private String nome;
 
+    public Integer getId() {
+        return id;
+    }
 
-    public Cliente(String nome) {
-        this.nome = nome;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getNome() {
---
+++ src/main/java/io/github/jaconunes/vendas/domain/repository/ClientesRepository.java
@@ -0,0 +1,20 @@
+package io.github.jaconunes.vendas.domain.repository;
+
+import io.github.jaconunes.vendas.domain.entity.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    private static String INSERT = "insert into cliente (nome) values (?) ";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public Cliente salvar(Cliente cliente){
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        return cliente;
+    }
+}

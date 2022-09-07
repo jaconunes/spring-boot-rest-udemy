4731306738c4b4202fbae2130a1d8da50308bcfd
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2022-09-07 17:04:43 -0300

    Utilizando o Controller Advice e os ExceptionHandlers para tratar erros na API

---
+++ src/main/java/io/github/jaconunes/vendas/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,21 @@
+package io.github.jaconunes.vendas.rest.controller;
+
+import io.github.jaconunes.vendas.exception.RegraNegocioException;
+import io.github.jaconunes.vendas.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex){
+        String mensagemErro = ex.getMessage();
+        return new ApiErrors(mensagemErro);
+    }
+
+}

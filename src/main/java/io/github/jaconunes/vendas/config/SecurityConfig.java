fbba5987218b70c3f9dbd013d27f5380c31a7d9e
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2022-09-11 14:55:11 -0300

    74. Implementação do UserDetailsService

--- src/main/java/io/github/jaconunes/vendas/config/SecurityConfig.java
+++ src/main/java/io/github/jaconunes/vendas/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.jaconunes.vendas.config;
 
+import io.github.jaconunes.vendas.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
+
     @Bean
     public PasswordEncoder passwordEncoder(){
        return new BCryptPasswordEncoder();
@@ -19,11 +25,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("fulano")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER");
+        auth.
+                userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override

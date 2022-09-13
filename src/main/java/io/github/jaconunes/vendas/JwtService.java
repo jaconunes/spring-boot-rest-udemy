c57674ebbedf6e69653edfe3ce7fd774d5b025fb
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2022-09-13 15:29:25 -0300

    79. Decodificando o token

--- src/main/java/io/github/jaconunes/vendas/JwtService.java
+++ src/main/java/io/github/jaconunes/vendas/JwtService.java
@@ -1,6 +1,8 @@
 package io.github.jaconunes.vendas;
 
 import io.github.jaconunes.vendas.domain.entities.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -32,5 +34,28 @@ public class JwtService {
                 .setExpiration(data)
                 .signWith(SignatureAlgorithm.HS512, chaveAssinatura)
                 .compact();
+    }
+
+    private Claims obterClaims( String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido ( String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExperiracao = claims.getExpiration();
+            LocalDateTime data = dataExperiracao.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data)
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario( String token ) throws  ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
     }
 }
